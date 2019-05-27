8cc9ce5f9971ce8169bc2fd4015d431b72cc98f0
Yıldırım Tam <yildirimtam@MacBook-Pro.local>
2019-05-27 17:50:11 +0300

    Initial commit

---
+++ app/src/main/java/io/github/simplemultilanguage/MainActivity.java
@@ -0,0 +1,64 @@
+package io.github.simplemultilanguage;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+
+            //Click English menu option
+            case R.id.action_english:
+                LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_ENGLISH);
+                recreate();
+                return true;
+
+            //Click French menu option
+            case R.id.action_french:
+                LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_FRENCH);
+                recreate();
+                return true;
+
+            //Click Russian menu option
+            case R.id.action_russian:
+                LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_RUSSIA);
+                recreate();
+                return true;
+
+            //Click Spanish menu option
+            case R.id.action_spanish:
+                LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_SPANISH);
+                recreate();
+                return true;
+
+            //Click Arabic menu option
+            case R.id.action_arabic:
+                LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_ARABIC);
+                recreate();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
