664e84cae687ee916a56982ca32576a416bcb64d
Yıldırım Tam <yildirimtam@MacBook-Pro.local>
2019-05-27 18:01:45 +0300

    Second commit

--- app/src/main/java/io/github/simplemultilanguage/MainActivity.java
+++ app/src/main/java/io/github/simplemultilanguage/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.simplemultilanguage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -31,34 +32,44 @@ public class MainActivity extends BaseActivity {
             //Click English menu option
             case R.id.action_english:
                 LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_ENGLISH);
-                recreate();
+
+                reLaunchApp();
                 return true;
 
             //Click French menu option
             case R.id.action_french:
                 LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_FRENCH);
-                recreate();
+                reLaunchApp();
                 return true;
 
             //Click Russian menu option
             case R.id.action_russian:
                 LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_RUSSIA);
-                recreate();
+                reLaunchApp();
                 return true;
 
             //Click Spanish menu option
             case R.id.action_spanish:
                 LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_SPANISH);
-                recreate();
+
+                reLaunchApp();
                 return true;
 
             //Click Arabic menu option
             case R.id.action_arabic:
                 LanguageManager.setNewLocale(this, LanguageManager.LANGUAGE_KEY_ARABIC);
-                recreate();
+
+
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void reLaunchApp(){
+
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
